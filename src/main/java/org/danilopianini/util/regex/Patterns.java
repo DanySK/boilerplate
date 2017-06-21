1c277a7e98c0a92095af063c14b9b175f48d9ae7
Danilo Pianini <danilo.pianini@unibo.it>
2017-06-21 14:44:07 +0200

    Add Patterns from Javalib-java7

---
+++ src/main/java/org/danilopianini/util/regex/Patterns.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (C) 2009, 2015, Danilo Pianini and contributors
+ * listed in the project's build.gradle or pom.xml file.
+ *
+ * This file is distributed under the terms of the Apache License, version 2.0
+ *******************************************************************************/
+package org.danilopianini.util.regex;
+
+import java.util.regex.Pattern;
+
+/**
+ * A set of pre-compiled {@link Pattern}s commonly used.
+ * 
+ */
+public final class Patterns {
+
+    /**
+     * If compiled, this {@link String} pattern matches any float or double BUT
+     * {@link Double#NaN}, {@link Double#POSITIVE_INFINITY} and
+     * {@value Double#NEGATIVE_INFINITY}.
+     */
+    public static final String FLOAT = "[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?";
+
+    /**
+     * This {@link Pattern} matches any float or double BUT {@link Double#NaN},
+     * {@link Double#POSITIVE_INFINITY} and {@value Double#NEGATIVE_INFINITY}.
+     */
+    public static final Pattern FLOAT_PATTERN = Pattern.compile(FLOAT);
+
+    private Patterns() {
+    }
+
+}
