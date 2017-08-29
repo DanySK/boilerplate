86b49cd7eee32194074ffd16923e354cb848613f
Danilo Pianini <danilo.pianini@unibo.it>
2017-08-29 13:43:03 +0200

    Update travis build config

--- travis_build.sh
+++ travis_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-openssl aes-256-cbc -K $encrypted_75ccbc63b5b6_key -iv $encrypted_75ccbc63b5b6_iv -in prepare_environment.sh.enc -out prepare_environment.sh -d
+openssl aes-256-cbc -K $encrypted_f6fa8cf5bc89_key -iv $encrypted_f6fa8cf5bc89_iv -in prepare_environment.sh.enc -out prepare_environment.sh -d
 bash prepare_environment.sh
 ./gradlew
 ./gradlew uploadArchives
