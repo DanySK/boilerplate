439acd381af8efa887a68f98c6b3f382d7f9e2f3
Danilo Pianini <danilo.pianini@unibo.it>
2019-05-15 16:37:17 +0200

    Only use the annotations module of spotbugs

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 
 dependencies {
     api("com.google.guava:guava:${extra["guavaVersion"]}")
-    implementation("com.github.spotbugs:spotbugs:${extra["spotbugsVersion"]}")
+    implementation("com.github.spotbugs:spotbugs-annotations:${extra["spotbugsVersion"]}")
     testImplementation("junit:junit:${extra["junitVersion"]}")
 }
 
