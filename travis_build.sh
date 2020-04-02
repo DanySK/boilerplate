e3c6e0994953e4db60573340c6811cc172133319
Danilo Pianini <danilo.pianini@unibo.it>
2020-04-02 20:55:57 +0200

    Delete travis_build.sh

--- travis_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-openssl aes-256-cbc -K $encrypted_f6fa8cf5bc89_key -iv $encrypted_f6fa8cf5bc89_iv -in prepare_environment.sh.enc -out prepare_environment.sh -d
-bash prepare_environment.sh
-./gradlew clean check
-./gradlew publish
